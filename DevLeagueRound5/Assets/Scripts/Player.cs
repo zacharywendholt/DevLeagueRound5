194eda60f12dd3424e0527c080b0fc632dc4fe39
zachary wendholt <37881315+zacharywendholt@users.noreply.github.com>
2022-08-26 19:30:10 -0400

    Added some basic baloon movement.

--- DevLeagueRound5/Assets/Scripts/Player.cs
+++ DevLeagueRound5/Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+    public float maxHorizontalSpeed = 3;
+    [SerializeField] int floatSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    private void FixedUpdate() {
+        // setting the float for testing purposes. 
+        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        float currentHorizontalVelocity = Vector2.Dot(rb.velocity, Vector2.right);
+        float currentVerticalVelocity = Vector2.Dot(rb.velocity, Vector2.up);
+
+        print(currentHorizontalVelocity + " : " +  currentVerticalVelocity);
+
+
+        
+        // After testing this needs to be changed to only allow horizontal movement. 
+        Vector2 newDirection = new Vector2(horizontalMovement * ( Mathf.Abs(maxHorizontalSpeed) - (horizontalMovement * currentHorizontalVelocity)), verticalMovement * floatSpeed);
+        rb.AddForce(newDirection, ForceMode2D.Force);
     }
 }
